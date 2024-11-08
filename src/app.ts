b3f104a4ef2a38f84673d395859d5cfa31679188
armelsteve1 <ouetchouaarmel@yahoo.com>
2024-11-08 16:36:26 +0100

    feat(): config vercel and firebase

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 // src/app.ts
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import catRoutes from './routes/catRoutes';
 import { loadCats } from './controllers/catController';
+import './config/firebase';
 
 const app = express();
 
@@ -11,7 +15,6 @@ app.use(cors());
 app.use(express.json()); 
 
 loadCats().then(() => console.log('Initial cat data loaded'));
-
 app.use('/api', catRoutes);
 
 export default app;
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,24 @@
 import admin from 'firebase-admin';
+import dotenv from 'dotenv';
 import serviceAccount from './firebaseServiceAccount.json';
 
+dotenv.config();
+
+const firebaseConfig = process.env.NODE_ENV === 'production'
+  ? {
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      }),
+    }
+  : {
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    };
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  databaseURL: "https://best-cat-fd089.firebaseio.com" 
+  ...firebaseConfig,
+  databaseURL: "https://best-cat-fd089.firebaseio.com",
 });
 
 export const db = admin.firestore();
